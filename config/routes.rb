fd12f557d145f6e7343abb0601c717a25961e3f3
Travis-Blair <travisblair74@gmail.com>
2018-03-07 11:23:46 -0500

    User profile page added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'users_controller/show'
   devise_for :users
   get 'welcome/index'
+
+  authenticated :user do
+    root 'users_controller#show', as: :authenticated_root
+  end
   root 'welcome#index'
 end
